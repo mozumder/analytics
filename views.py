6c986b3135ca0cf3d781b18b4b564f1c6bf0e3f9
Bobby Mozumder <mozumder@futureclaw.com>
2018-10-19 07:13:56 -0400

    Latest analytics

--- views.py
+++ views.py
@@ -7,10 +7,8 @@ from .signals import *
 
 class AnalyticResponseBase():
 
-    def __init__(self, request=None, slug=None, id=None, cached=False,*args, **kwargs):
+    def __init__(self, request=None, cached=False,*args, **kwargs):
         self.request = request
-        self.slug = slug
-        self.id = id
         self.cached = cached
         super().__init__(*args, **kwargs)
 
@@ -30,55 +28,55 @@ class AnalyticResponseBase():
 
 class AnalyticResponse(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, request=None, slug=None, id=None, cached=None, content=b'', content_type=None, status=200, reason=None, charset=None):
-        super().__init__(request, slug, id, cached, content, content_type, status, reason, charset)
+    def __init__(self, request=None, cached=None, content=b'', content_type=None, status=200, reason=None, charset=None):
+        super().__init__(request, cached, content, content_type, status, reason, charset)
 
 class HTMLAnalyticResponse(AnalyticResponseBase, HttpResponse):
 
     def __init__(self, content=b'', status=200, reason=None, charset=None, request=None, slug=None, id=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
 class StreamingHTMLAnalyticResponse(AnalyticResponseBase, StreamingHttpResponse):
 
-    def __init__(self, streaming_content=(), status=200, reason=None, charset=None, request=None, slug=None, id=None, pageKey=None):
+    def __init__(self, streaming_content=(), status=200, reason=None, charset=None, request=None, pageKey=None):
         self.pageKey = pageKey
-        super().__init__(request, slug, id, cached=False, streaming_content=streaming_content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached=False, streaming_content=streaming_content, status=status, reason=reason, charset=charset)
 
 class JSONAnalyticResponse(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=200, reason=None, charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=200, reason=None, charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, "application/json", status, reason, charset)
+        super().__init__(request, cached, content, "application/json", status, reason, charset)
 
 
 class AnalyticsHttpResponseNotFound(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=404, reason='Not Found', charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=404, reason='Not Found', charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
 class AnalyticsHttpResponseBadRequest(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=400, reason='Bad Request', charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=400, reason='Bad Request', charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
 class AnalyticsHttpResponseForbidden(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=403, reason='Forbidden', charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=403, reason='Forbidden', charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
 class AnalyticsHttpResponseGone(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=410, reason='Gone', charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=410, reason='Gone', charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
 class AnalyticsHttpResponseServerError(AnalyticResponseBase, HttpResponse):
 
-    def __init__(self, content=b'', status=500, reason='Server Error', charset=None, request=None, slug=None, id=None, cached=False):
+    def __init__(self, content=b'', status=500, reason='Server Error', charset=None, request=None, cached=False):
         self.length = len(content)
-        super().__init__(request, slug, id, cached, content, status=status, reason=reason, charset=charset)
+        super().__init__(request, cached, content, status=status, reason=reason, charset=charset)
 
