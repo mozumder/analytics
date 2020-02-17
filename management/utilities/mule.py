8629cd5fdebd489dcc6914609f0fa6fed567a8ab
Bobby Mozumder <mozumder@futureclaw.com>
2020-02-17 15:54:14 -0500

    DB Cursors sync

--- management/utilities/mule.py
+++ management/utilities/mule.py
@@ -7,35 +7,16 @@ from analytics.management.utilities.logger import LogWriter
 from uwsgi import mule_get_msg
 
 logger = logging.getLogger("django")
-dblogger = logging.getLogger("database")
 
-lock=threading.Lock()
+_lock=threading.Lock()
 cursor=connection.cursor()
-
-file_name = 'analytics/include/sql/prepare.sql'
-try:
-    file = open(file_name, 'r')
-except FileNotFoundError:
-    dblogger.debug('No SQL file: %s' % file_name)
-    pass
-except (OSError, IOError) as e:
-    dblogger.error('Error reading SQL file: %s' % file_name)
-    raise e
-else:
-    sql_commands=file.read().strip()
-    if sql_commands:
-        try:
-            cursor.execute('deallocate log;')
-        except:
-            pass
-        cursor.execute(sql_commands)
 
 def listener():
     global cursor
     global lock
     logwriter = LogWriter(
         cursor=cursor,
-        lock=lock
+        lock=_lock
     )
     logger.info("uWSGI Log Mule Process started & listening")
     while True:
